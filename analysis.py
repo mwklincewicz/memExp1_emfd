766dac7a07e5f4f8daf5644925f9961264f610b5
mwklincewicz <62137998+mwklincewicz@users.noreply.github.com>
2024-10-04 22:21:21 +0200

    first

--- analysis.py
+++ analysis.py
@@ -18,10 +18,10 @@ pd.set_option('display.max_rows', 400)
 
 #exp1_df = pd.read_excel("./test.xlsx")
 
-exp1_df = pd.read_excel("./rawdata_exp1.xlsx")
-#exp2_df = pd.read_excel("./rawdata_exp2.xlsx")
+expA_df = pd.read_excel("./rawdata_exp1.xlsx")
+expB_df = pd.read_excel("./rawdata_exp2.xlsx")
 
-#exp1_df = pd.concat([exp1_df, exp2_df] )
+exp1_df = pd.merge(expA_df, expB_df, how="outer")
 
 my_translator = GoogleTranslator(source='auto', target='en')
 my_nl_translator = GoogleTranslator(source='auto', target='nl')
